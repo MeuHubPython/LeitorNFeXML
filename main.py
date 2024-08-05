e2caac2dac5e23564af17d5c44fbd7d41f992955
MeuHubPython <gustavotitara070@gmail.com>
2024-08-05 20:32:25 -0300

    Primeiro Commit

---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+Projeto de um leitor de XML usando a biblioteca xmltodict, pandas e tkinter
+
+- O sistema lerá arquivos DANFE em busca de informações específicas
+- imprimirá essas informações numa GUI feita com TKinter
+- O sistema terá a opção de criar um arquivo excel com os dados retirados das DANFES
+"""
+
+import xmltodict
+
